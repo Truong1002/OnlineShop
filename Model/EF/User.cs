4c6cbd7c4da2060c1bb6121d951587d19b17e937
Truong <truongphamxuan724@gmail.com>
2023-11-08 19:30:53 +0700

    Add project files.

---
+++ Model/EF/User.cs
@@ -0,0 +1,55 @@
+﻿namespace Model.EF
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("User")]
+    public partial class User
+    {
+        public long ID { get; set; }
+
+        [StringLength(50)]
+        [DisplayName("Tài Khoản")]
+        public string UserName { get; set; }
+        [DisplayName("Mật Khẩu")]
+        [StringLength(32)]
+        public string Password { get; set; }
+        [DisplayName("Họ Tên")]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [DisplayName("Địa Chỉ")]
+        [StringLength(50)]
+        public string Address { get; set; }
+
+        [StringLength(50)]
+
+        public string Email { get; set; }
+
+        [StringLength(50)]
+        [DisplayName("Điện Thoại")]
+        public string Phone { get; set; }
+
+        public int? ProvinceID { get; set; }
+
+        public int? DistrictID { get; set; }
+
+        public int? PrecinctID { get; set; }    
+
+        public DateTime? CreatedDate { get; set; }
+
+        [StringLength(50)]
+        public string CreatedBy { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+        [StringLength(50)]
+        public string ModifiedBy { get; set; }
+
+        [DisplayName("Trạng Thái")]
+        public bool Status { get; set; }
+    }
+}
---
+++ OnlineShop/assets/client/js/controller/userController.js
@@ -0,0 +1,88 @@
+﻿var user = {
+    init: function () {
+        user.loadProvince();
+        user.registerEvent1();
+        user.registerEvent2();
+    },
+    registerEvent1: function () {
+        $('#ddlProvince').off('change').on('change', function () {
+            var id = $(this).val();
+            if (id != '') {
+                user.loadDistrict(parseInt(id));
+            } else {
+                $('#ddlDistrict').html(''); 
+            }
+        }); 
+    },
+
+    registerEvent2: function () {
+        $('#ddlDistrict').off('change').on('change', function () {
+            var districtID = $(this).val();
+            var provinceID = $('#ddlProvince').val(); // Lấy giá trị của tỉnh từ dropdown tỉnh
+            if (districtID != '') {
+                user.loadPrecinct(parseInt(districtID), parseInt(provinceID)); // Truyền cả hai biến
+            } else {
+                $('#ddlPrecinct').html('');
+            }
+        });
+    },
+    loadProvince: function () {
+        
+        $.ajax({
+            url: '/User/LoadProvince',
+            type: "POST",
+            dataType: "json",
+            success: function (response) {
+                if (response.status == true) {
+                    var html = '<option value="">--Chọn tỉnh thành--</opton>';
+                    var data = response.data;
+                    $.each(data, function (i, item) {
+                        html += '<option value = "' + item.ID + '">' + item.Name + '</option>'
+                    });
+                    $('#ddlProvince').html(html);
+                }
+            }
+        })
+    },
+
+    loadDistrict: function (id) {
+
+        $.ajax({
+            url: '/User/LoadDistrict',
+            type: "POST",
+            data: {provinceID: id},
+            dataType: "json",
+            success: function (response) {
+                if (response.status == true) {
+                    var html = '<option value="">--Chọn quận huyện--</opton>';
+                    var data = response.data;
+                    $.each(data, function (i, item) {
+                        html += '<option value = "' + item.ID + '">' + item.Name + '</option>'
+                    });
+                    $('#ddlDistrict').html(html);
+                }
+            }
+        })
+    },
+
+    loadPrecinct: function (districtID, provinceID) {
+
+        $.ajax({
+            url: '/User/LoadPrecinct',
+            type: "POST",
+            data: { districtID: districtID, provinceID: provinceID },
+            dataType: "json",
+            success: function (response) {
+                if (response.status == true) {
+                    var html = '<option value="">--Chọn xã--</opton>';
+                    var data = response.data;
+                    $.each(data, function (i, item) {
+                        html += '<option value = "' + item.ID + '">' + item.Name + '</option>'
+                    });
+                    $('#ddlPrecinct').html(html);
+                }
+            }
+        })
+    }
+}
+user.init();
